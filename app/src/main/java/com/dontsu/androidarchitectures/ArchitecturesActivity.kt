c3e70e4e5a144985c4afc73c492b1222ef94856a
dauni6 <45280927+dauni6@users.noreply.github.com>
2020-05-23 22:58:00 +0900

    Initial commit

---
+++ app/src/main/java/com/dontsu/androidarchitectures/ArchitecturesActivity.kt
@@ -0,0 +1,12 @@
+package com.dontsu.androidarchitectures
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ArchitecturesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_architectrues)
+    }
+}
